6df708a8843ca5b7f7a11f8925ac9681849a785d
vori1987 <riabokon.vladimir@gmail.com>
2023-09-02 17:31:34 +0200

    add exercise 8 - grouping and counting

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -16,7 +16,13 @@ public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
+    Map<String, List<Car>> grouping = MockData.getCars().stream()
+        .collect(Collectors.groupingBy(Car::getMake));
 
+    grouping.forEach((make, cars) -> {
+      System.out.println(make);
+      cars.forEach(System.out::println);
+    });
   }
 
   @Test
@@ -34,7 +40,9 @@ public class Lecture8 {
             "Alex"
         );
 
+    Map<String, Long> collecting = names.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
+    collecting.forEach((name, count) -> System.out.println(name + " > " + count + ";"));
   }
-
 }
