8ea842a3e0c7af6345b3fe35a9c4ebea80976fb5
vori1987 <riabokon.vladimir@gmail.com>
2023-09-02 16:46:50 +0200

    add exercise 5 - change type of data on another

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -7,7 +7,9 @@ import beans.Car;
 import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
+import java.sql.SQLOutput;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import mockdata.MockData;
@@ -18,7 +20,14 @@ public class Lecture5 {
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
+    final Predicate<Car> carPredicate = car -> car.getPrice() < 20000;
 
+    List<Car> carsFiltered = cars.stream()
+        .filter(carPredicate)
+        .collect(Collectors.toList());
+
+    carsFiltered.forEach(System.out::println);
+    System.out.println(carsFiltered.size());
   }
 
   @Test
@@ -26,17 +35,30 @@ public class Lecture5 {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
 
+    List<PersonDTO> dtos = people.stream()
+        .map(PersonDTO::map)
+        .collect(Collectors.toList());
+
+    dtos.forEach(System.out::println);
+
+    assertThat(dtos).hasSize(1000);
+    System.out.println(dtos.size());
   }
 
   @Test
   public void averageCarPrice() throws Exception {
     // calculate average of car prices
-
+    double average = MockData.getCars()
+        .stream()
+        .mapToDouble(Car::getPrice)
+        .average()
+        .orElse(0);
+    System.out.println(average);
   }
 
   @Test
   public void test() throws Exception {
-
+MockData.getCars().forEach(System.out::println);
   }
 }
 
