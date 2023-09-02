3e967a04ebe30036f95c565becdc5e1dbc337f46
vori1987 <riabokon.vladimir@gmail.com>
2023-09-02 17:12:07 +0200

    add exercise 7 - find max/min/average

--- src/test/java/lectures/Lecture7.java
+++ src/test/java/lectures/Lecture7.java
@@ -8,6 +8,7 @@ import com.google.common.collect.ImmutableList;
 import java.math.BigDecimal;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
@@ -16,24 +17,42 @@ public class Lecture7 {
 
   @Test
   public void count() throws Exception {
-
+    long count = MockData.getPeople()
+        .stream()
+        .filter(person -> person.getGender().equalsIgnoreCase("Female"))
+        .count();
+    System.out.println(count);
   }
 
   @Test
   public void min() throws Exception {
-
+    double min = MockData.getCars()
+        .stream().filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+        .mapToDouble(Car::getPrice)
+        .min()
+        .orElse(0);
+    System.out.println(min);
   }
 
   @Test
   public void max() throws Exception {
-
+    double max = MockData.getCars()
+        .stream().filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+        .mapToDouble(Car::getPrice)
+        .max()
+        .orElse(0);
+    System.out.println(max);
   }
 
 
   @Test
   public void average() throws Exception {
     List<Car> cars = MockData.getCars();
-
+    double averagePrice = cars.stream()
+        .mapToDouble(Car::getPrice)
+        .average()
+        .orElse(0);
+    System.out.println(averagePrice);
   }
 
   @Test
