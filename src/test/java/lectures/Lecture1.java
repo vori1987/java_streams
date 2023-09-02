35048da464e51207ff99fccb0bee1eaa25a785bf
vori1987 <riabokon.vladimir@gmail.com>
2023-09-02 15:15:07 +0200

    add exercise 1 and 2 (streams)

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -6,6 +6,7 @@ import beans.Person;
 
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,12 +22,22 @@ public class Lecture1 {
     List<Person> people = MockData.getPeople();
     // 1. Find people aged less or equal 18
     // 2. Then change implementation to find first 10 people
+    int count = 0;
 
+    for (Person person : people) {
+      if (person.getAge() <= 18) {
+        System.out.println(person.getFirstName() + " is " + person.getAge() + " old is less 18y");
+        count++;
+      }
+      if (count >= 10) {
+        break;
+      }
+    }
   }
 
   @Test
   public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
+    MockData.getPeople().stream().filter(person -> person.getAge() <= 18)
+        .limit(10).collect(Collectors.toList()).forEach(System.out::println);
   }
 }
--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -10,20 +10,31 @@ public class Lecture2 {
 
   @Test
   public void range() throws Exception {
+    System.out.println("  for i");
+    for (int i = 0; i <= 10; i++) {
+      System.out.println(i);
+    }
+    System.out.println("  exclusive");
+    IntStream.range(0, 10).forEach(System.out::println);
 
+    System.out.println("  inclusive");
+    IntStream.rangeClosed(0, 10).forEach(System.out::println);
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
     List<Person> people = MockData.getPeople();
-
+    IntStream.range(0, people.size()).forEach(index -> {
+      Person person = people.get(index);
+      System.out.println(person);
+    });
   }
 
   @Test
   public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
+    IntStream.iterate(0, operand -> operand + 1)
+        .filter(number -> number % 2 == 0)
+        .limit(20)
+        .forEach(System.out::println);
   }
 }
