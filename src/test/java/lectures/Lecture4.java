6fcc5c13dae2fe1623a7dfb0e44e0c2e81471e74
vori1987 <riabokon.vladimir@gmail.com>
2023-09-02 15:55:57 +0200

    add exercise 4 - distinct in Set and List collection (replace repeated ints)

--- src/test/java/lectures/Lecture4.java
+++ src/test/java/lectures/Lecture4.java
@@ -13,13 +13,19 @@ public class Lecture4 {
 
   @Test
   public void distinct() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
-
+    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8,
+        9, 9, 9, 9, 9);
+    List<Integer> distinctNumbers = numbers.stream().distinct().collect(Collectors.toList());
+    assertThat(distinctNumbers).hasSize(9);
+    System.out.println(distinctNumbers);
   }
 
   @Test
   public void distinctWithSet() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
-
+    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8,
+        9, 9, 9, 9, 9);
+    Set<Integer> distinctWithSet = numbers.stream().collect(Collectors.toSet());
+    assertThat(distinctWithSet).hasSize(9);
+    System.out.println(distinctWithSet);
   }
 }
